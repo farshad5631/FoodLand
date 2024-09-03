e518a0846483b1b35949398b5d4faa3173bc595b
farshad5631 <farshadkalhor88@gmail.com>
2024-09-03 20:02:01 +0330

    student icon added

---
+++ src/assets/icons/Student.tsx
@@ -0,0 +1,43 @@
+
+const Student = () => {
+  return (
+    <svg
+      width="81"
+      height="80"
+      viewBox="0 0 81 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.5 20V45"
+        stroke="#404040"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M17.4375 67.5C19.9389 63.6633 23.3581 60.5113 27.3851 58.3295C31.4122 56.1477 35.9199 55.0051 40.5 55.0051C45.0801 55.0051 49.5879 56.1477 53.6149 58.3295C57.642 60.5113 61.0611 63.6633 63.5625 67.5"
+        stroke="#404040"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M70.5 20L40.5 30L10.5 20L40.5 10L70.5 20Z"
+        stroke="#404040"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M53.4063 25.6875C55.6995 28.1918 57.2147 31.3092 57.767 34.6597C58.3193 38.0101 57.8849 41.4489 56.5168 44.5568C55.1487 47.6647 52.9059 50.3074 50.062 52.1628C47.218 54.0182 43.8957 55.0061 40.5 55.0061C37.1043 55.0061 33.782 54.0182 30.938 52.1628C28.0941 50.3074 25.8513 47.6647 24.4832 44.5568C23.1151 41.4489 22.6807 38.0101 23.233 34.6597C23.7854 31.3092 25.3005 28.1918 27.5938 25.6875"
+        stroke="#404040"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Student;
